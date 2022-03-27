5beba58296f5a6894763ea1df52a817a443c437d
zuoliang <193957056@qq.com>
2022-03-28 01:04:49 +0800

    文件名修改

---
+++ vue3-demo/src/15-mixins-App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container1">
+    <h1> 作者：周杰伦  <a href="javascript:;" @click="followFn">{{loading?'正在请求。。。':'关注'}}</a> </h1>
+    <hr>
+    <son />
+  </div>
+</template>
+<script>
+import son from './son.vue'
+import {followMixin} from './mixins.js'
+export default {
+  name: 'App',
+  components: {
+    son
+  },
+  mixins:[followMixin]
+}
+</script>
